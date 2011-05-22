7327492eeea4ca7172b1eb0afa5fba91a476bdd4
Nicolas Pantano <npanti@gmail.com>
2011-05-22 14:25:16 +0200

    Modification du programme fonctionne maintenant

--- modul_fm.m
+++ modul_fm.m
@@ -1,19 +1,18 @@
 %% Initialisation
 clc;
 clear;
+plt = 50;
 
 F = 500;            %Fréquence symbole
-T = 1/F;
 Fs = 20000;         %Fréquence d'échantillonnage Matlab
-Ts = 1/Fs;
 OSF = Fs/F;
-bits_size = 1000;   %Nombre de symbole émis
+bits_size = 10;   %Nombre de symbole émis
 Kf = 500;           %Sélectivité fréquentielle
 Fc = 6000;          %Fréquence porteuse
-Tc = 1/Fc;
-vector_size = OSF*bits_size;
 
-t =0:Ts:OSF*Ts*bits_size-Ts;
+t=0:1/Fs:OSF*bits_size*1/Fs-1/Fs;
+
+longueur_chaine = bits_size*OSF;
 
 %% Création des bits
 bits = randi(2,1, bits_size) - 1;   %-1 pour avoir 0 ou 1 en aléatoire
@@ -36,95 +35,66 @@ for i=1:bits_size
 end
 
 figure(1);
-plot(msg,'-', 'LineWidth', 3); hold on;
+plot(msg,'-','LineWidth',3);
 
 %% Modulation FM
-%Es = fmmod(msg,Fc,Fs,50); ou modulate
-% somme = spalloc(1,40000,40000);
-% for n=1:40000
-%     somme(1,n) = sum(msg(1:n))/Fs;
-% end
 
 %Cumsum est la somme cumulée, c'est l'intégrale discrète de notre message
-e_s = exp(1i*2*pi*Kf*cumsum(msg));
-
-% figure(2);
-% plot(t,real(e_s),'-'); hold on;
-% plot(t,imag(e_s),'-r');
-% plot(t,msg,'xg','MarkerSize',10);
+e_s = exp(1i*2*pi*Kf*cumsum(msg)/Fs);
 
 
 %% Signal RF
 
 s = real(e_s).*cos(2*pi*Fc*t) - imag(e_s).*sin(2*pi*Fc*t);
 
-% figure(3);
-% plot(t,s); hold on;
-% plot(t,msg,'xg','MarkerSize',10);
-
-s2 = fmmod(msg,Fc,Fs,Kf);
-
-figure(2);
-plot(t,s,'-or');
- hold on;
-plot(t,s2,'-xb');
+% s2 = fmmod(msg,Fc,Fs,Kf);
+% 
+% plot(t,s,'-or');
+% hold on;
+% plot(t,s2,'-xb');
 
 %% Bruit blanc
 
-n = zeros(1,vector_size);
+n = zeros(1, longueur_chaine);
 
 r = s + n;
 
-%% Filtre passe-bas
-%Optention des deux signal qui vont rentrer dans le filtre passe-bas
+%% Filtre passe bas
 
 r_cos = r.*cos(2*pi*Fc*t);
 r_sin = r.*sin(2*pi*Fc*t);
 
-
-% subplot(211); hold on;
-% subplot(212); hold on;
-% rcosfir(0,20,10,1/(3*Fc),'r');
-
-h = rcosfir(0,20,10,1/(3*Fc));
+h = rcosfir(0,10,20,1/(3*Fc));
 
 e_r = conv(r_cos,h) + 1i*conv(r_sin,h);
-e_r = e_r(20*10+1:vector_size+20*10);
 
+e_r = e_r(1+10*20:longueur_chaine+20*10);
 
-%% Démodulation FSK non cohérente
+%% Démodulateur FSK
 
 e_s0 = exp(-1i*2*pi*Kf*t);
 e_s1 = exp(1i*2*pi*Kf*t);
 
 for k=1:bits_size
     
-    sum_s0 = 0;
-    sum_s1 = 0;
-    
+    s0_sum = 0;
+    s1_sum = 0;
+     
     for i=1:OSF
-        sums_s0 = sum_s0 + e_r((k-1)*OSF+i)*conj(e_s0((k-1)*OSF+i));
-        sums_s1 = sum_s1 + e_r((k-1)*OSF+i)*conj(e_s1((k-1)*OSF+i));
+        s0_sum = s0_sum + e_r((k-1)*OSF+i)*conj(e_s0((k-1)*OSF+i));
+        s1_sum = s1_sum + e_r((k-1)*OSF+i)*conj(e_s1((k-1)*OSF+i));
     end
     
-    if abs(sums_s0) < abs(sums_s1)
+    if abs(s0_sum) > abs(s1_sum)
         m(1,k) = 1;
     else
         m(1,k) = 0;
     end
-    
-%     if  abs(sum(e_r((k-1)*OSF+1:k*OSF).*conj(e_s0((k-1)*OSF+1:k*OSF)))) > abs(sum(e_r((k-1)*OSF+1:k*OSF).*conj(e_s1((k-1)*OSF+1:k*OSF))))
-%         m2(1,k) = 0;
-%     else
-%         m2(1,k) = 1;
-%     end
 end
 
-figure(1);
-hold on;
+%% Comparaison des résultats
 
-msg = [];   %Définition de notre matrisse message = m(t)
-
+msg = [];
 for i=1:bits_size
    
     bit = m(1,i);
@@ -139,19 +109,5 @@ for i=1:bits_size
     
 end
 
+hold on;
 plot(msg,'-r');
-
-
-%m = abs(cumsum(e_r.*conj(e_s1))) - abs(cumsum(e_r.*conj(e_s0)));
-% for t2 = 1:size(m,2)
-%     if m(1,t2) > 0
-%         m(1,t2) = 1;
-%     else
-%         m(1,t2) = 0;
-%     end
-% end
-
-
-
-%% Démodulation FM
-
